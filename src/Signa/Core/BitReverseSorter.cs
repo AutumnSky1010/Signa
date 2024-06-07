87c5d21bc3941d9f109ba3b8f7cec0653715c8a0
AutumnSky1010 <66455966+AutumnSky1010@users.noreply.github.com>
2024-06-07 12:47:46 +0900

    :+1: Improve sorting algorithm.

--- src/Signa/Core/BitReverseSorter.cs
+++ src/Signa/Core/BitReverseSorter.cs
@@ -3,21 +3,20 @@ internal static class BitReverseSorter
 {
     public static void Sort<T>(T[] signal)
     {
+        int halfCount = signal.Length / 2;
         int i = 0;
-        int k;
-
-        for (int j = 1; j < signal.Length - 1; j++)
+        for (int j = 0; j < halfCount; j += 2)
         {
-            k = signal.Length / 2;
-            while (k <= i)
-            {
-                i -= k;
-                k /= 2;
-            }
-            i += k;
             if (j < i)
             {
                 (signal[i], signal[j]) = (signal[j], signal[i]);
+                (signal[i + halfCount + 1], signal[j + halfCount + 1]) = (signal[j + halfCount + 1], signal[i + halfCount + 1]);
+            }
+
+            (signal[j + halfCount], signal[i + 1]) = (signal[i + 1], signal[j + halfCount]);
+
+            for (int k = halfCount >> 1; k > (i ^= k); k >>= 1)
+            {
             }
         }
     }
