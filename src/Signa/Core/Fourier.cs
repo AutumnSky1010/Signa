2aae23177eff7c351c1cc01c7484beceae6818d0
AutumnSky1010 <66455966+AutumnSky1010@users.noreply.github.com>
2024-06-08 23:04:15 +0900

    :recycle: Improve amount of calculation

--- src/Signa/Core/Fourier.cs
+++ src/Signa/Core/Fourier.cs
@@ -1,22 +1,32 @@
 ﻿using System.Numerics;
 
 namespace Signa.Core;
-public static class Fourier
+public class Fourier
 {
     public static Complex[] FFT(int n, int[] signal)
+    public Fourier(int n)
     {
-        if (!BitOperations.IsPow2(n))
+        if (!BitOperations.IsPow2(N))
         {
             throw new ArgumentException("");
         }
+        _twiddleFactorForFFT = CalculateTwiddleFactor(n);
+        N = n;
+    }
 
-        if (signal.Length > n)
+    public int N { get; }
+
+    public Complex[] FFT(int[] signal)
         {
-            signal = signal.Take(n).ToArray();
+        // 指定したNを超えている場合は信号を切り取る。
+        if (signal.Length > N)
+        {
+            signal = signal.Take(N).ToArray();
         }
-        else if (signal.Length < n)
+        // 指定したNより小さい信号が与えられた場合は0-paddingする
+        else if (signal.Length < N)
         {
-            var newArray = new int[n];
+            var newArray = new int[N];
             int i;
             for (i = 0; i < signal.Length; i++)
             {
@@ -29,18 +39,22 @@ public static class Fourier
             signal = newArray;
         }
 
-        var twiddleFactor = CalculateTwiddleFactor(n);
+        // ビットリバースソートし、結果用配列に格納する
         BitReverseSorter.Sort(signal);
-        var result = new Complex[n];
+        var result = new Complex[N];
         for (int i = 0; i < result.Length; i++)
         {
             result[i] = new Complex(signal[i], 0);
         }
         FFTInternal(0, n, twiddleFactor, result);
 
+        // N点FFTを実行
+        FFTInternal(0, N, _twiddleFactorForFFT, result);
+
+        // 1/Nを乗ずる
         for (int i = 0; i < result.Length; i++)
         {
-            result[i] *= 1.0d / n;
+            result[i] *= 1.0d / N;
         }
         return result;
     }
@@ -48,24 +62,33 @@ public static class Fourier
     private static void FFTInternal(int startIndex, int n, Complex[] twiddleFactor, Complex[] result)
     {
         int halfCount = n / 2;
+        // 2点まで分解してFFTする
         if (halfCount != 1)
         {
             FFTInternal(startIndex, halfCount, twiddleFactor, result);
             FFTInternal(startIndex + halfCount, halfCount, twiddleFactor, result);
         }
 
-
+        // result用のindex
         int index = startIndex;
+        // 回転因子用のindex
         int twiddleIndex = 0;
+        // 1ループあたり回転因子のインデクスの増加量
         int twiddleIndexCommonDifference = result.Length / n;
         for (int i = 0; i < halfCount; i++)
         {
-            int oddIndex = index + halfCount;
+            // result[index]と対となるresultのインデクス
+            int pairedIndex = index + halfCount;
+            // twddle[twiddleIndex](回転因子)と対となる回転因子のインデクス
+            int pairedTwiddleIndex = twiddleIndex + halfCount * twiddleIndexCommonDifference;
 
+            // resultを書き換えるのであらかじめ記憶する
             var old = result[index];
-            var oldOdd = result[oddIndex];
-            result[index] = old + twiddleFactor[twiddleIndex] * oldOdd;
-            result[oddIndex] = old + twiddleFactor[twiddleIndex + halfCount * twiddleIndexCommonDifference] * oldOdd;
+            var oldPaired = result[pairedIndex];
+
+            // バタフライ演算を行う
+            result[index] = old + twiddleFactor[twiddleIndex] * oldPaired;
+            result[pairedIndex] = old + twiddleFactor[pairedTwiddleIndex] * oldPaired;
 
             index++;
             twiddleIndex += twiddleIndexCommonDifference;
